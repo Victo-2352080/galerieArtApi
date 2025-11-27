eb5c12139a24315d675ac34fc2b563e01af731a4
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2025-11-27 14:33:03 -0500

    please

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import logger from 'jet-logger';
+import { connect } from 'mongoose';
 
 import ENV from '@src/common/constants/ENV';
 import server from './server';
-import { connect } from 'mongoose';
 
 /******************************************************************************
                                 Constants
@@ -15,6 +15,7 @@ const SERVER_START_MSG =
                                   Run
 ******************************************************************************/
 
+// Start the server
 connect(ENV.Mongodb).then(() =>
   server.listen(ENV.Port, () => logger.info(SERVER_START_MSG)),
 );
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import helmet from 'helmet';
 import express, { Request, Response, NextFunction } from 'express';
 import logger from 'jet-logger';
-import cors from 'cors';
 
 import BaseRouter from '@src/routes';
 
@@ -12,6 +11,7 @@ import ENV from '@src/common/constants/ENV';
 import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
 import { RouteError } from '@src/common/util/route-errors';
 import { NodeEnvs } from '@src/common/constants';
+import cors from 'cors';
 
 /******************************************************************************
                                 Setup
@@ -22,21 +22,27 @@ const app = express();
 // **** Middleware **** //
 app.use(cors());
 
+// Basic middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Show routes called in console during development
 if (ENV.NodeEnv === NodeEnvs.Dev) {
   app.use(morgan('dev'));
 }
 
+// Security
 if (ENV.NodeEnv === NodeEnvs.Production) {
+  // eslint-disable-next-line n/no-process-env
   if (!process.env.DISABLE_HELMET) {
     app.use(helmet());
   }
 }
 
+// Add APIs, must be after middleware
 app.use(Paths.Base, BaseRouter);
 
+// Add error handler
 app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   if (ENV.NodeEnv !== NodeEnvs.Test.valueOf()) {
     logger.err(err, true);
@@ -48,5 +54,29 @@ app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   }
   return next(err);
 });
+
+// **** FrontEnd Content **** //
+
+// Set views directory (html)
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+
+// Set static directory (js and css).
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+
+// Nav to users pg by default
+app.get('/', (_: Request, res: Response) => {
+  return res.redirect('/users');
+});
+
+// Redirect to login if not logged in.
+app.get('/users', (_: Request, res: Response) => {
+  return res.sendFile('users.html', { root: viewsDir });
+});
+
+/******************************************************************************
+                                Export default
+******************************************************************************/
 
 export default app;

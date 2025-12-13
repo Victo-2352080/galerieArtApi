3085838abdc862cdab66cb46495cd675deb2efa3
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2025-12-13 11:10:43 -0500

    fix#3

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,27 @@
 import logger from 'jet-logger';
 import { connect } from 'mongoose';
-
 import ENV from '@src/common/constants/ENV';
 import server from './server';
 
-/******************************************************************************
-                                Constants
-******************************************************************************/
-
+// **** Constants **** //
 const SERVER_START_MSG =
   'Express server started on port: ' + ENV.Port.toString();
 
-/******************************************************************************
-                                  Run
-******************************************************************************/
+// **** Run **** //
 
-// Start the server
-connect(ENV.Mongodb).then(() =>
-  server.listen(ENV.Port, () => logger.info(SERVER_START_MSG)),
-);
+logger.info(`Attempting to connect to MongoDB at: ${ENV.Mongodb}`);
+
+connect(ENV.Mongodb)
+  .then(() => {
+    logger.info('MongoDB connected successfully.');
+
+    server.listen(ENV.Port, '0.0.0.0', () => {
+      logger.info(SERVER_START_MSG);
+    });
+  })
+  .catch((err) => {
+    logger.err('MongoDB connection failed!', true);
+    logger.err(err, true);
+
+    process.exit(1);
+  });

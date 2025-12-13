36e41d6c46c810bb196dc1ecfa51f32317d1a236
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2025-12-13 11:12:58 -0500

    fix#4

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,23 @@ logger.info(`Attempting to connect to MongoDB at: ${ENV.Mongodb}`);
 connect(ENV.Mongodb)
   .then(() => {
     logger.info('MongoDB connected successfully.');
-
+    // On écoute bien sur 0.0.0.0 pour Azure
     server.listen(ENV.Port, '0.0.0.0', () => {
       logger.info(SERVER_START_MSG);
     });
   })
-  .catch((err) => {
+  .catch((err: unknown) => {
+    // CORRECTION 1 : On type explicitement en 'unknown'
     logger.err('MongoDB connection failed!', true);
-    logger.err(err, true);
 
+    // On vérifie si c'est bien une erreur avant de la logger pour satisfaire TypeScript
+    if (err instanceof Error) {
+      logger.err(err, true);
+    } else {
+      logger.err(String(err), true);
+    }
+
+    // CORRECTION 2 : On dit au linter d'ignorer cette règle juste pour la ligne suivante
+    // eslint-disable-next-line n/no-process-exit
     process.exit(1);
   });

a94b3b4e6232ba9c5ea8b94ac81752c15ed4559c
NathanGrondin <2352080@carrefour.cegepvicto.ca>
2025-12-13 21:13:51 -0500

    temp

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,8 @@ import ENV from '@src/common/constants/ENV';
 import HttpStatusCodes from '@src/common/constants/HttpStatusCodes';
 import { RouteError } from '@src/common/util/route-errors';
 import { NodeEnvs } from '@src/common/constants';
-import authenticateToken from './services/authenticateToken';
+import authenticateToken from './services/temp_name';
 import cors from 'cors';
-
 
 /******************************************************************************
                                 Setup
@@ -21,13 +20,12 @@ import cors from 'cors';
 
 const app = express();
 
-
 // **** Middleware **** //
 app.use(cors());
 
 // Basic middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Show routes called in console during development
 if (ENV.NodeEnv === NodeEnvs.Dev) {
@@ -60,7 +58,6 @@ app.use((err: Error, _: Request, res: Response, next: NextFunction) => {
   return next(err);
 });
 
-
 // **** FrontEnd Content **** //
 
 // Set views directory (html)
@@ -80,7 +77,6 @@ app.get('/', (_: Request, res: Response) => {
 app.get('/users', (_: Request, res: Response) => {
   return res.sendFile('users.html', { root: viewsDir });
 });
-
 
 /******************************************************************************
                                 Export default
